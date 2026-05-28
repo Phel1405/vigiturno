579912dc187b15b49ec1c85897173cb5c7d9a19c
Phel <149622001+Phel1405@users.noreply.github.com>
2026-05-28 00:51:06 -0500

    -Intento de Google login

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  googleClientId: 'TU_CLIENT_ID_DE_GOOGLE.apps.googleusercontent.com'
+  production: false
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: false,
-  googleClientId: 'TU_CLIENT_ID_DE_GOOGLE.apps.googleusercontent.com'
+  production: false
 };
